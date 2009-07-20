42154a362a00690591609d612897277648c98162
Corey Ling <kasuko@gmail.com>
2009-07-20 09:55:09 -0400

    Added Sakura and small changes.

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -17,7 +17,7 @@ beautiful.init(theme_path)
 require("obvious.volume_alsa")
 
 -- This is used later as the default terminal and editor to run.
-terminal = "urxvt"
+terminal = "sakura"
 editor = os.getenv("EDITOR") or "vi"
 editor_cmd = terminal .. " -e " .. editor
 
